65784c651cf72849008e5b5e529d7f7c942c4c3d
Chiranthana R R <73631715+chiru30@users.noreply.github.com>
2021-05-22 23:36:40 +0530

    🍂🍁

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ if rad == "Home":
     q.image("/Users/chiranthanarr/portfolio/0C3B8359-4504-45D3-91DD-0BE600C0B26F_4_5005_c.jpeg")
     st.markdown("""
     ### I am a computer science engineering student at SRMIST , passionate and enthusiastic about exploring the field data science and web scraping deeper.""",True)
+    
+    st.header("Connect me")
+    m,n,o=st.beta_columns(3)
+    m.header('[LINKEDIN](https://www.linkedin.com/in/chiranthana-r-r-232385200/)')
+    m.image('174857.png')
+    n.header('[GITHUB](https://github.com/chiru30/)')
+    n.image('25231.png')
+    o.header('[MEDIUM](https://chiranthana30rr.medium.com)')
+    o.image('unnamed.png')
