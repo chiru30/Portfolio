2eeab5575071a387c93192bb2c26ade9dc37b85b
Chiranthana R R <73631715+chiru30@users.noreply.github.com>
2021-05-24 00:08:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+import os
+import base64
+
 st.set_page_config(layout="wide")
 
 rad=st.sidebar.radio("Menu",["Home","Education and Experience","Projects","Technologies and Tools"])
@@ -18,9 +21,27 @@ if rad == "Home":
     
     st.header("Connect me")
     m,n,o=st.beta_columns(3)
-    m.header('[LINKEDIN](https://www.linkedin.com/in/chiranthana-r-r-232385200/)')
-    m.image('174857.png')
-    n.header('[GITHUB](https://github.com/chiru30/)')
-    n.image('25231.png')
-    o.header('[MEDIUM](https://chiranthana30rr.medium.com)')
-    o.image('unnamed.png')
+    
+    @st.cache(allow_output_mutation=True)
+    def get_base64_of_bin_file(bin_file):
+        with open(bin_file, 'rb') as f:
+            data = f.read()
+            return base64.b64encode(data).decode()
+    @st.cache(allow_output_mutation=True)
+    def get_img_with_href (local_img_path, target_url):
+        img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
+        bin_str = get_base64_of_bin_file(local_img_path)
+        html_code = f'''
+        <a href="{target_url}">
+            <img src="data:image/{img_format};base64,{bin_str}" />
+        </a>'''
+        return html_code
+    
+    medium_html = get_img_with_href('medium.png', 'https://chiranthana30rr.medium.com')
+    o.markdown(medium_html, unsafe_allow_html=True)
+
+    linkedin_html = get_img_with_href('linkedin_logo.png', 'https://www.linkedin.com/in/chiranthana-r-r-232385200/')
+    m.markdown(linkedin_html, unsafe_allow_html=True)
+
+    github_html = get_img_with_href('github.png', 'https://github.com/chiru30/')
+    n.markdown(github_html, unsafe_allow_html=True)
